db920e244cf75fddd38791e33564ded068f04db6
hextran <104662919+hextran@users.noreply.github.com>
2022-06-18 16:19:11 +0700

    Create foreach.php

---
+++ foreach.php
@@ -0,0 +1,16 @@
+<?php
+
+define ("enter","<br>");
+
+/**
+$colors = array("vàng","xanh","tím");
+foreach ($colors as $x){
+  echo $x . enter;
+}
+**/
+
+
+$info = array("myName"=>"Trọng Hòa","Age"=>19);
+foreach ($info as $a => $z){
+  echo $a .": ". $z . enter; 
+}
